5cfa48f4e8a09734ee8797e6388e7f6625e1d39f
lyfQAQ <0219lyf@gmail.com>
2024-08-27 22:05:09 +0800

    do some propagate fn

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,6 +1,51 @@
-pub struct Network;
+#[derive(Debug)]
+pub struct Network {
+    layers: Vec<Layer>,
+}
 
-impl Network {}
+impl Network {
+    pub fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |acc, layer| layer.propagate(acc))
+    }
+}
+
+#[derive(Debug)]
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+}
+
+#[derive(Debug)]
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>, // weights的长度表示输出个数
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(a, b)| a * b)
+            .sum::<f32>();
+        (output + self.bias).max(0.0)
+    }
+}
 
 #[cfg(test)]
 mod tests {
