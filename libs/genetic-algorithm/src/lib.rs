7c80901f4170eb7b8d059be0f2ac957f340fab60
Liuyifan <0219lyf@gmail.com>
2024-08-28 21:21:04 +0800

    add Chromosome

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,4 +1,5 @@
-use rand::{seq::SliceRandom, Rng, RngCore};
+use rand::{seq::SliceRandom, RngCore};
+use std::ops::Index;
 
 pub struct GeneticAlgorithm<S> {
     selection_method: S,
@@ -21,6 +22,7 @@ impl<S: SelectionMethod> GeneticAlgorithm<S> {
 
 pub trait Individual {
     fn fitness(&self) -> f32;
+    fn chromosome(&self) -> &Chromosome;
 }
 
 pub trait SelectionMethod {
@@ -47,6 +49,49 @@ impl SelectionMethod for RouletteWheelSelection {
     }
 }
 
+/// 染色体由基因(权重)组成
+#[derive(Clone, Debug)]
+pub struct Chromosome {
+    genes: Vec<f32>,
+}
+
+impl Chromosome {
+    pub fn len(&self) -> usize {
+        self.genes.len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &f32> {
+        self.genes.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut f32> {
+        self.genes.iter_mut()
+    }
+}
+
+impl Index<usize> for Chromosome {
+    type Output = f32;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.genes[index]
+    }
+}
+
+impl FromIterator<f32> for Chromosome {
+    fn from_iter<T: IntoIterator<Item = f32>>(iter: T) -> Self {
+        Self {
+            genes: iter.into_iter().collect(),
+        }
+    }
+}
+
+impl IntoIterator for Chromosome {
+    type Item = f32;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.genes.into_iter()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use rand::SeedableRng;
@@ -70,6 +115,9 @@ mod tests {
     impl Individual for TestIndividual {
         fn fitness(&self) -> f32 {
             self.fitness
+        }
+        fn chromosome(&self) -> &Chromosome {
+            panic!("not supported for TestIndividual")
         }
     }
     #[test]
